7e4ce19466777dc1459552c8f3d9c25729ec204f
Hwang seo yeon <80025812+seoyeonne@users.noreply.github.com>
2023-04-24 13:52:14 +0900

    [Silver V] Title: 숫자 카드, Time: 804 ms, Memory: 115404 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+nrr = list(map(int,sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+mrr = list(map(int,sys.stdin.readline().split()))
+
+dic ={}
+
+for i in mrr:
+    dic[i] = 0
+
+for j in nrr:
+    if j in dic:
+        dic[j] = 1
+
+for i in dic:
+    print(dic[i],end= " ")
